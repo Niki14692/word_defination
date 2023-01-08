d3562d26340e302f1ba4f902c86288f5d69df593
niki <niku17patel@gmail.com>
2023-01-08 08:34:31 +0530

    day-30 add csv file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def home():
 
 @app.route('/api/v1/<word>/')
 def api(word):
-    definition = word.upper()
+    df = pd.read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word]['definition'].squeeze()
     result_dictionary = {'word': word, 'definition': definition}
     return result_dictionary
 
